71db8c6dbdf7d5b9ca25822f677d04fc9a5b0375
Nguyen Anh Tien <nguyenanhtien2210@gmail.com>
2017-05-11 08:39:37 +0700

    For DN branch

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
  * Created by dinhtungtp on 4/20/2017.
  */
 var googleCalendar = {
-    API_KEY : 'Google API',
-    Calendar_Id: 'Calendar ID'
+    API_KEY : 'AIzaSyC94E9YH_ldRbfjiGKTkc5-7gbmNLfKzao',
+    Calendar_Id: 'fdn.bo.no2@gmail.com'
 }
--- js/init.js
+++ js/init.js
@@ -3,27 +3,9 @@
  */
 // initiate color, each room corresponds to a color
 var colors = {
-        'Abuja': '#E53935',
-        'Astana': '#8E24AA',
-        'Ban tron': '#3949AB',
-        'Bangkok': '#00ACC1',
-        'Cebu': '#43A047',
-        'CEO Room': '#FFB300',
-        'Dhaka': '#F4511E',
-        'DN Office': '#E53935',
-        'Jakarta': '#3949AB',
-        'Kuala Lumpur': '#00ACC1',
-        'Other': '#43A047',
-        'Phnompenh': '#FFB300',
-        'Shimane': '#F4511E',
-        'Singapore': '#E53935',
-        'Tokyo': '#8E24AA',
-        'Toong': '#3949AB',
-        'Vientiane': '#00ACC1',
-        'Booth 1': '#43A047',
-        'Booth 2': '#FFB300',
-        'Booth 3': '#F4511E',
-        'Booth 4': '#E53935',
+        'Atlantic': '#E53935',
+        'Arctic': '#8E24AA',
+        'Pacific': '#3949AB',
+        'Indian': '#00ACC1',
     },
-    rooms = ['Abuja', 'Astana', 'Ban tron', 'Bangkok', 'Cebu', 'CEO Room', 'Dhaka', 'DN Office', 'Jakarta', 'Kuala Lumpur'
-        ,'Phnompenh', 'Shimane', 'Singapore', 'Tokyo', 'Toong', 'Vientiane', 'Booth 1', 'Booth 2', 'Booth 3', 'Booth 4', 'Other'];
+    rooms = ['Atlantic', 'Arctic', 'Pacific', 'Indian'];
